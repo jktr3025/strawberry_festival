012b9d7edd1f86e9b6032e1c045744a55296a6d1
jktr3025 <jktr3025@naver.com>
2024-02-14 20:09:04 +0900

    festival day1

---
+++ js/my.script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+$(".full").fullpage({
+    keyboardScrolling:true,       
+    ScrollingSpeed:3000,  
+    menu:"#menu", 
+    achors:["#section0", "#section1","#section2","#section3", "#section4", "#section5"] ,
+})
+
+
+
+
+
+
+
+
+
+})//jquery
