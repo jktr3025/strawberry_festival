12415d9ef0b8bf717f77578af565f0c54eb988ff
jktr3025 <jktr3025@naver.com>
2024-02-20 20:32:31 +0900

    festival day3

--- js/my.script.js
+++ js/my.script.js
@@ -4,7 +4,7 @@ $(".full").fullpage({
     keyboardScrolling:true,       
     ScrollingSpeed:3000,  
     menu:"#menu", 
-    achors:["#section0", "#section1","#section2","#section3", "#section4", "#section5"] ,
+    anchors:["section0", "section1","section2","section3", "section4", "section5"] ,
     afterLoad :function(index){
         var n =index.index;
         console.log("n :", n)
